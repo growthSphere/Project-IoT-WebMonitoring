f9bbcd6128a9e3bdfaf04d842cba64a933652c5e
growthSphere <iqbalakram273@gmail.com>
2024-12-08 00:42:29 +0700

    koneksi ke database

---
+++ PHP/koneksi.php
@@ -0,0 +1,14 @@
+<?php 
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "dbmonitor";
+
+    $koneksi = mysqli_connect($server, $username, $password, $database);
+    //if ($koneksi == TRUE){
+       // echo"Terhubung ke database";
+    //}
+    //else{
+        //echo "Tidak terhubung ke database";
+    //}
+?>
